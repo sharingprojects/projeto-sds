15bf6cdf21e118fafbfd2805063687a58a7caa8b
Andreza Telles Cascais <shareingprojects@gmail.com>
2021-05-10 00:15:05 -0300

    Routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+  
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,4 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
