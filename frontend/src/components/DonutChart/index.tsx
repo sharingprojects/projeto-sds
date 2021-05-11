49be4977d71f66ac8264867daa4f0c2aafbd7bc7
Andreza Telles Cascais <shareingprojects@gmail.com>
2021-05-10 21:10:11 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
@@ -9,42 +10,27 @@ type ChartData = {
 };
 
 const DonutChart = () => {
-  //FORM ERRADA
-  let chartData: ChartData = { labels: [], series: [] };
 
-  //pode ser assim:
-  // axios.get(BASE_URL + '/sales/amount-by-seller');
+  const [chartData, setCharData] = useState<ChartData>({ labels: [], series: [] });
 
-  //FORM ERRADA
-  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
-    const data = response.data as SaleSum[];
-    const myLabels = data.map((x) => x.sellerName);
-    const mySeries = data.map((x) => x.sum);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+  
+      setCharData({ labels: myLabels, series: mySeries });     
+    });
 
-    chartData = { labels: myLabels, series: mySeries };
-    console.log(chartData);
-  });
-
-  /* axios.get(`${BASE_URL}/sales/amount-by-seller`)
-      .then(response => {
-          const data = response.data as SaleSum[];
-          const myLabels = data.map(x => x.sellerName);
-          const mySeries = data.map(x => x.sum);
-
-          chartData = ( labels: myLabels, series: mySeries );
-          console.log(chartData);
-      });*/
+  }, []);
+ 
+  
 
   const options = {
     legend: {
       show: true,
     },
   };
-
-  //const mockData = {
-  //   series: [477138, 499928, 444867, 220426, 473088],
-  //   labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  // };
 
   return (
     <Chart
